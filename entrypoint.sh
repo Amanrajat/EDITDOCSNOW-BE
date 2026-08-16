9d5c19b139eb293256793ccf06148bf838e08af1
Aman Srivastav <amansrivastava072@gmail.com>
2026-08-16 13:00:50 +0530

    fix: Correct shebang and remove commented-out lines in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,4 @@
-# #!/bin/sh
-# set -e
-
-# python manage.py migrate --noinput
-# python manage.py collectstatic --noinput
-
-# exec gunicorn core.wsgi:application \
-#     --bind 0.0.0.0:${PORT:-8000} \
-#     --workers ${WEB_CONCURRENCY:-3} \
-#     --timeout 120
-
-
-!/bin/sh
+#!/bin/sh
 set -e
 
 python manage.py migrate --noinput
