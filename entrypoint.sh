366fd4b9a71bbaf1d1d1a04243eda7ac2868325e
Aman Srivastav <amansrivastava072@gmail.com>
2026-08-16 12:54:09 +0530

    feat: Add Celery worker startup to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,25 @@
+# #!/bin/sh
+# set -e
+
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
+
+# exec gunicorn core.wsgi:application \
+#     --bind 0.0.0.0:${PORT:-8000} \
+#     --workers ${WEB_CONCURRENCY:-3} \
+#     --timeout 120
+
+
 #!/bin/sh
 set -e
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# Start Celery worker in background
+celery -A core worker --loglevel=info &
+
+# Start Django web server
 exec gunicorn core.wsgi:application \
     --bind 0.0.0.0:${PORT:-8000} \
     --workers ${WEB_CONCURRENCY:-3} \
